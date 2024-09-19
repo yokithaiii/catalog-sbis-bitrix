b77c42c486817e20d641283f1d902721c5516d5b
spxrtxk <spartakvasilev1999@gmail.com>
2024-09-19 15:19:48 +0900

    feat: add product interface

---
+++ src/Interface/ProductInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Yokithaii\CatalogSbisBitrix\Interface;
+
+interface ProductInterface
+{
+    public function getId(): int;
+
+    public function getName(): string;
+
+    public function getPrice(): float;
+
+    public function getDescription(): string;
+
+    public function setId(int $id): void;
+
+    public function setName(string $name): void;
+
+    public function setPrice(float $price): void;
+
+    public function setDescription(string $description): void;
+
+}
